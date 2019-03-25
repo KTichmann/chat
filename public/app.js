ddabd0867c2c3133d3744a6ef3e5dadf1090fefd
unknown <ktichmann@gmail.com>
2019-03-25 12:56:50 +0200

    add enter to chat

--- public/app.js
+++ public/app.js
@@ -1,21 +1,31 @@
 //get username --> listen to io chat messages, push them to the ul
 //
 
-if(io && username){
-    const socket = io();
+if (io && username) {
+	const socket = io();
 
-    document.querySelector('#chatApp button').addEventListener('click', () => {
-        let message = document.querySelector('#chatApp input').value;
-        if(message !== ""){
-            console.log(message);
-            socket.emit('chat message', {username: username, message: message});
-        }
-        document.querySelector('#chatApp input').value = ''
-    })
+	document.querySelector("#chatApp button").addEventListener("click", () => {
+		sendMessage();
+	});
 
-    socket.on('chat message', function(data){
-        let li = document.createElement('li')
-        li.textContent = `${data.username}: ${data.message}`
-        document.querySelector('#chatApp ul').appendChild(li)
-      });
+	window.addEventListener("keyup", event => {
+		if (event.isComposing || event.keyCode === 229) {
+			return;
+		} else if (event.keyCode === 13) {
+			sendMessage();
+		}
+	});
+
+	function sendMessage() {
+		let message = document.querySelector("#chatApp input").value;
+		if (message !== "") {
+			socket.emit("chat message", { username: username, message: message });
+		}
+		document.querySelector("#chatApp input").value = "";
+	}
+	socket.on("chat message", function(data) {
+		let li = document.createElement("li");
+		li.textContent = `${data.username}: ${data.message}`;
+		document.querySelector("#chatApp ul").appendChild(li);
+	});
 }
